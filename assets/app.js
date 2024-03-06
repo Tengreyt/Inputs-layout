0f7ad93fa7ca9b9901d74f2e43c999aedefef70f
Tengreyt <halid.taramov20004@gmail.com>
2024-03-06 20:38:58 +0300

    first commit

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+let box = document.querySelector(".block"),
+    input = document.querySelector("input");
+
+ input.addEventListener("input", () => {
+    box.style.borderRadius = input.value;
+    box.style.background = input.value;
+    // box.style.width = input.value;
+ });
